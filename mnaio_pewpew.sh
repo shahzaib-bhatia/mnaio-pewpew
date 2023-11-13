d347aa604ee419e2d827c61aeaddd5cc44a74f15
Shahzaib Bhatia <shahzaib.bhatia@rackspace.com>
2023-11-13 04:51:25 -0600

    Update mnaio_pewpew.sh

--- mnaio_pewpew.sh
+++ mnaio_pewpew.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
+if [ "$(id -u)" -ne 0 ]; then
+  echo 'be root pls'
+  exit 1
+fi
+
+#### config ####
+DEVSTACK_BRANCH="stable/yoga"
+WANT_DISTRIB_CODENAME="focal"
+
+#### helpers ####
+
+reboot_and_rerun () {
+  chmod +x $(realpath $0)
+  cat > /etc/systemd/system/mnaio_pewpew.service <<EOF
+[Service]
+ExecStart=$(realpath $0)
+[Install]
+WantedBy=default.target
+EOF
+  systemctl daemon-reload
+  systemctl enable mnaio_pewpew.service
+  reboot
+}
+
+
+#### be nice ####
+if (systemctl -q is-enabled mnaio_pewpew.service 2>&-) ; then
+  systemctl disable mnaio_pewpew.service
+fi
+
+if [[ -f /etc/systemd/system/mnaio_pewpew.service ]] ; then
+  rm /etc/systemd/system/mnaio_pewpew.service
+fi
+
+
 #### do it in a tmux ####
 
 if [[ ! -n ${TMUX} ]] ; then
@@ -18,79 +53,65 @@ set -o pipefail
 
 #### get space ####
 
-if [[ ! $(pvs /dev/sdc) ]] ; then
-  pvcreate /dev/sdc
-fi
-if [[ ! $(pvs /dev/sdd) ]] ; then
-  pvcreate /dev/sdd
-fi
-if [[ ! $(vgs vg_data) ]] ; then
-  vgcreate vg_data /dev/sdc /dev/sdd
-fi
-if [[ ! $(lvs vg_data/lv_data 2>&-) ]] ; then
-  lvcreate --type raid0 -l 25%FREE --nosync -n lv_data vg_data
-fi
-if [[ ! $(blkid /dev/vg_data/lv_data) ]] ; then
-  mkfs.ext4 /dev/vg_data/lv_data
-fi
-
-mkdir -p /data
-
-if [[ ! $(grep -q ^/dev/vg_data/lv_data /etc/fstab) ]] ; then
-  echo '/dev/vg_data/lv_data /data auto defaults,,noatime,barrier=0 0 2' >> /etc/fstab
-fi
-
-mount -a
+#if [[ ! $(pvs /dev/sdc) ]] ; then
+#  pvcreate /dev/sdc
+#fi
+#if [[ ! $(pvs /dev/sdd) ]] ; then
+#  pvcreate /dev/sdd
+#fi
+#if [[ ! $(vgs vg_data) ]] ; then
+#  vgcreate vg_data /dev/sdc /dev/sdd
+#fi
+#if [[ ! $(lvs vg_data/lv_data 2>&-) ]] ; then
+#  lvcreate --type raid0 -l 25%FREE --nosync -n lv_data vg_data
+#fi
+#if [[ ! $(blkid /dev/vg_data/lv_data) ]] ; then
+#  mkfs.ext4 /dev/vg_data/lv_data
+#fi
+#
+#mkdir -p /data
+#
+#if [[ ! $(grep -q ^/dev/vg_data/lv_data /etc/fstab) ]] ; then
+#  echo '/dev/vg_data/lv_data /data auto defaults,,noatime,barrier=0 0 2' >> /etc/fstab
+#fi
+#
+#mount -a
 
 #### update stuff ####
 
 apt update
-apt -y upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" dist-upgrade -q -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
 
 if [[ -f /var/run/reboot-required ]] ; then
   cat /var/run/reboot-required
-  reboot
+  reboot_and_rerun
 fi
 
-#### canonical pls ####
+source /etc/lsb-release
 
-apt -y install nginx python3-pip
-mkdir -p /var/www/pxe/ubuntu
-if [[ ! -f /var/www/pxe/ubuntu/ubuntu-22.04.3-live-server-amd64.iso ]] ; then
-  cd /var/www/pxe/ubuntu
-  wget http://releases.ubuntu.com/jammy/ubuntu-22.04.3-live-server-amd64.iso
-fi
-TMPDIR=$(mktemp -d)
-mount /var/www/pxe/ubuntu/ubuntu-22.04.3-live-server-amd64.iso ${TMPDIR}
-cp ${TMPDIR}/casper/{vmlinuz,initrd} /var/www/pxe/ubuntu/
-mv /var/www/pxe/ubuntu/vmlinuz /var/www/pxe/ubuntu/linux
-gzip -f /var/www/pxe/ubuntu/initrd
-umount ${TMPDIR}
-rmdir ${TMPDIR}
-
-#### start work ####
-
-if [[ -d /opt/openstack-ansible-ops ]] ; then
-  rm -rvf /opt/openstack-ansible-ops
+if [[ "${DISTRIB_CODENAME}" != "${WANT_DISTRIB_CODENAME}" ]] ; then
+  do-release-upgrade -m server -f DistUpgradeViewNonInteractive
+  reboot_and_rerun
 fi
 
-git clone https://github.com/shahzaib-bhatia/openstack-ansible-ops.git /opt/openstack-ansible-ops
+apt -y install pwgen
 
-#### set vars ####
-export MNAIO_ANSIBLE_PARAMETERS=""
-export MNAIO_ANSIBLE_PARAMETERS="${MNAIO_ANSIBLE_PARAMETERS} -e osa_enable_networking_ovs_dvr=true"
-export MNAIO_ANSIBLE_PARAMETERS="${MNAIO_ANSIBLE_PARAMETERS} -e osa_no_containers=true"
+mkdir -p /opt/stack
+if [[ -d /opt/stack/devstack ]] ; then
+  rm -rvf /opt/stack/devstack
+fi
+git clone https://opendev.org/openstack/devstack /opt/stack/devstack
+bash /opt/stack/devstack/tools/create-stack-user.sh
+cd /opt/stack/devstack
+git checkout "${DEVSTACK_BRANCH}"
 
-export ENABLE_CEPH_STORAGE="true"
-export DEFAULT_IMAGE="ubuntu-22.04-amd64"
-export OSA_BRANCH="stable/yoga"
-export COMPUTE_VM_SERVER_RAM="16384"
+cat > "/opt/stack/devstack/local.conf" <<EOF
+[[local|localrc]]
+ADMIN_PASSWORD=$(pwgen -s 31 1)
+DATABASE_PASSWORD=$(pwgen -s 31 1)
+RABBIT_PASSWORD=$(pwgen -s 31 1)
+SERVICE_PASSWORD=$(pwgen -s 31 1)
+EOF
 
-#### patch ####
-# TODO: do the good git thing
-sed -i 's/openstack.cloud.os_nova_flavor/openstack.cloud.compute_flavor/' /opt/openstack-ansible-ops/multi-node-aio/playbooks/openstack-service-setup.yml
-sed -i 's/openstack.cloud.os_/openstack.cloud./' /opt/openstack-ansible-ops/multi-node-aio/playbooks/openstack-service-setup.yml
-
-cd /opt/openstack-ansible-ops/multi-node-aio
-
-./build.sh
+chown -R stack:stack /opt/stack
+sudo -u stack /opt/stack/devstack/stack.sh
