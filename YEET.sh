d7f084029cfa56a82bc2a7b85ee74b06ed9c5b31
Shahzaib Bhatia <shahzaib.bhatia@rackspace.com>
2024-02-06 14:59:26 +0000

    allow saving of LVs (for cache preservation)

--- YEET.sh
+++ YEET.sh
@@ -1,8 +1,25 @@
+VM_SAVE_DIR="/var/lib/pewpew"
+SAVE_LVS="lv_utility1_1 lv_utility1_2"
+
 if [[ $(type -t deactivate) == "function" ]] ; then  deactivate ; fi
 
+for VM in $(virsh list --uuid) ; do
+  virsh shutdown --mode acpi ${VM}
+done
+
+until [[ "$(virsh list | grep -c running)" -eq 0 ]] ; do
+  echo Being nice...
+  virsh list --all
+  sleep 1
+done
+
 for VM in $(virsh list --uuid --all) ; do
-  virsh destroy ${VM}
   virsh undefine ${VM}
+done
+
+for LV in ${SAVE_LVS} ; do
+  echo "Saving ${LV} ..."
+  pv -B 16M "/dev/vg_libvirt/${LV}" | pigz > "${VM_SAVE_DIR}/${LV}.gz"
 done
 lvremove -y vg_libvirt
 
